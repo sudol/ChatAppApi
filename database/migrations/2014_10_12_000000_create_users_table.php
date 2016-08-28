c2f39adffe241c39d6eaac589436382bb0695b19
Paul Sudol <paulsudol@gmail.com>
2016-08-27 22:49:40 -0500

    Add an API token to the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('api_token', 255)->unique();
             $table->rememberToken();
             $table->timestamps();
         });
